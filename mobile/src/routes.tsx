141aa7860ad9631c5f5970f5560997d3772f341e
Lorran Sutter <sutterlorran@gmail.com>
2020-10-16 17:03:12 -0400

    Mobile interface | integration

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -4,15 +4,48 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanageMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
-                <Screen name='OrphanagesMap' component={OrphanageMap} />
-                <Screen name='OrphanageDetails' component={OrphanageDetails} />
+            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#F2F3F5' } }}>
+                <Screen
+                    name='OrphanagesMap'
+                    component={OrphanageMap}
+                />
+
+                <Screen
+                    name='OrphanageDetails'
+                    component={OrphanageDetails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title='Orphanage' />
+                    }}
+                />
+
+                <Screen
+                    name='SelectMapPosition'
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title='Select on the map' />
+                    }}
+                />
+
+                <Screen
+                    name='OrphanageData'
+                    component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title='Inform the data' />
+                    }}
+                />
+
             </Navigator>
         </NavigationContainer>
     )
